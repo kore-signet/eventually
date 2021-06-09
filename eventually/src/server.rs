8c17136803ad68f0696bf7ffb4c6129306bcc27e
Allie Signet <allie@cat-girl.gay>
2021-06-08 21:15:16 -0300

    switch to rocket

--- eventually/src/server.rs
+++ eventually/src/server.rs
@@ -1,60 +1,74 @@
+use chrono::prelude::*;
 use compass::*;
-use actix_web::{get, web, App, HttpServer, HttpResponse, Error, middleware::Logger};
-use deadpool_postgres::{Config, ManagerConfig, Client, Pool, RecyclingMethod };
-use tokio_postgres::{NoTls};
+use serde_json::json;
+use serde_json::Value as JSONValue;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
-use serde_json::Value as JSONValue;
-use serde_json::json;
-use chrono::prelude::*;
-use actix_cors::Cors;
 
-#[get("/events")]
-async fn search(mut req: web::Query<JSONValue>, db_pool: web::Data<Pool>, schema: web::Data<Schema>) -> Result<HttpResponse,Error> {
-    let client: Client = db_pool.get().await.map_err(CompassError::PoolError)?;
+use rocket::fairing::{self, Fairing};
+use rocket::{get, http::Header, launch, routes, Request, Response};
+use rocket_sync_db_pools::{database, postgres};
 
+#[database("eventually")]
+struct CompassConn(postgres::Client);
+
+struct CORS;
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> fairing::Info {
+        fairing::Info {
+            name: "CORS headers",
+            kind: fairing::Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "GET"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+    }
+}
+
+#[get("/events?<req..>")]
+async fn search(
+    mut req: HashMap<String, String>,
+    db: CompassConn,
+    schema: Schema,
+) -> Result<JSONValue, CompassError> {
     if let Some(mut before) = req.get_mut("before") {
-        if before.as_str().unwrap().parse::<i64>().is_err() {
-            *before = json!(before.as_str().unwrap().parse::<DateTime<Utc>>().unwrap().timestamp().to_string());
+        if before.parse::<i64>().is_err() {
+            *before = before
+                .parse::<DateTime<Utc>>()
+                .unwrap()
+                .timestamp()
+                .to_string();
         }
     }
 
     if let Some(mut after) = req.get_mut("after") {
-        if after.as_str().unwrap().parse::<i64>().is_err() {
-            *after = json!(after.as_str().unwrap().parse::<DateTime<Utc>>().unwrap().timestamp().to_string());
+        if after.parse::<i64>().is_err() {
+            *after = after
+                .parse::<DateTime<Utc>>()
+                .unwrap()
+                .timestamp()
+                .to_string();
         }
     }
 
-    let res = json_search(&client,&schema,&req).await.map(|val| json!(val))?;
-    Ok(HttpResponse::Ok().json(res))
+    db.run(move |mut c| json_search(&mut c, &schema, &req).map(|val| json!(val)))
+        .await
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    env_logger::init();
-
-    let cfg = Config::from_env("PG").unwrap();
-    let pool = cfg.create_pool(NoTls).unwrap();
-
+#[launch]
+fn rocket() -> _ {
     let mut file = File::open("schema.yaml").unwrap();
     let mut s = String::new();
     file.read_to_string(&mut s).unwrap();
     let schema: Schema = serde_yaml::from_str(&s).unwrap();
-
-    let server = HttpServer::new(move || {
-        let cors = Cors::default()
-                    .allow_any_origin()
-                    .allowed_methods(vec!["GET"]);
-
-        App::new()
-            .data(pool.clone())
-            .data(schema.clone())
-            .wrap(cors)
-            .wrap(Logger::default())
-            .service(search)
-    })
-    .bind("0.0.0.0:4445".to_string())?
-    .run();
-
-    server.await
+    rocket::build()
+        .manage(schema)
+        .attach(CompassConn::fairing())
+        .attach(CORS)
+        .mount("/", routes![search])
 }
